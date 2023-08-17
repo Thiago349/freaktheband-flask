02d091ddc8911062f615f19787208bca24d8b0aa
Thiago349 <thiagoreis2002@gmail.com>
2023-08-17 19:37:38 -0300

    CAdeleted

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app.config['MYSQL_HOST'] = os.getenv('HOST')
 app.config['MYSQL_USER'] = os.getenv('USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('DATABASE')
-app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/DigiCertGlobalRootCA.crt.pem"}} 
 
 mysql = MySQL(app)
 
