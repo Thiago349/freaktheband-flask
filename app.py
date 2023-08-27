14a68b3c11a25b05464ea570df057c28f2f3ee8e
Thiago349 <thiagoreis2002@gmail.com>
2023-08-26 22:33:22 -0300

    AppTest18

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def rocksession_db():
 def contact_email():
     if request.method == "POST":
         print("OI")
-        print(request.data)
+        print(request.data['data'])
         msg = email.message.Message()
         '''
         content = f"""
